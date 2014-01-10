922b85c1900a3746ee52d756e0881fff8b2d3c0f
gryphus <gryphus.d@gmail.com>
2014-01-10 01:02:20 +0100

    9.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+
+
+  def index
+    @user = User.all
+  end
 
 	def show
     	@user = User.find(params[:id])
@@ -20,7 +27,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
 
@@ -44,6 +51,14 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
